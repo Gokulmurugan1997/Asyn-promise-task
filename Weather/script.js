cc3d00bf7433b6a432a1d2724c4e16fe87f125f2
Gokulmurugan1997 <Gokulanshvi@gmail.com>
2023-11-24 22:54:21 +0530

    update

--- Weather/script.js
+++ Weather/script.js
@@ -1,39 +1,39 @@
-function weatherdata(){
-    let promise = new Promise((resolve,reject)=>{
-        let cityName = document.getElementById('cityName').value;
-        let request= fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a43292ebbf1482b46db85b4a44f4a740`);
-        
-        if(request.status==200){
-                var data = request.then((data2)=>data2.json()).then((data1)=>{console.log(data1)})
-                
-        resolve(data)
-            }
-            else{
-                reject("some error occured")
-            }
-        
-    })
-    promise.then((data)=>{console.log(data)}).catch((err)=>{console.log(err)})
-    }weatherdata()
-
 // function weatherdata(){
-// let promise = new Promise((resolve,reject)=>{
+//     let promise = new Promise((resolve,reject)=>{
+//         let cityName = document.getElementById('cityName').value;
 
-//     var request = new XMLHttpRequest();
-//     let cityName = document.getElementById('cityName').value;
-//     request.open('GET',`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a43292ebbf1482b46db85b4a44f4a740`);
-    
-//     request.send();
-   
-//     request.onload = function(){
+//         let request= fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a43292ebbf1482b46db85b4a44f4a740`);
+
+//         var data = request.then((data2)=>data2.json()).then((data1)=>{console.log(data1)})
 //         if(request.status==200){
-//             var data = JSON.parse(request.response)
-//     resolve(data)
-//         }
-//         else{
-//             reject("some error occured")
-//         }
-//     }
-// })
-// promise.then((data)=>{console.log(data)}).catch((err)=>{console.log(err)})
-// }weatherdata()
+//         resolve(data)
+//             }
+//             else{
+//                 reject("some error occured")
+//             }
+        
+//     })
+//     promise.then((data)=>{console.log(data)}).catch((err)=>{console.log(err)})
+//     }weatherdata()
+
+function weatherdata(){
+let promise = new Promise((resolve,reject)=>{
+
+    var request = new XMLHttpRequest();
+    let cityName = document.getElementById('cityName').value;
+    request.open('GET',`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a43292ebbf1482b46db85b4a44f4a740`);
+    
+    request.send();
+   
+    request.onload = function(){
+        if(request.status==200){
+            var data = JSON.parse(request.response)
+    resolve(data)
+        }
+        else{
+            reject("some error occured")
+        }
+    }
+})
+promise.then((data)=>{console.log(data)}).catch((err)=>{console.log(err)})
+}weatherdata()
