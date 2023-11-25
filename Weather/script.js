5291c591e822ea85d5eb04435061cb92ae8e2166
Gokulmurugan1997 <Gokulanshvi@gmail.com>
2023-11-25 11:56:08 +0530

    update

--- Weather/script.js
+++ Weather/script.js
@@ -1,8 +1,11 @@
 function weatherData(){
-    
+    let promise = new Promise((resolve, reject) => {
+        
+  
         var cityName = document.getElementById('cityName').value;
 
         var res= fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a43292ebbf1482b46db85b4a44f4a740`);
+        console.log(res)
         res.then((data2)=>data2.json()).then((data1)=>{console.log(data1)
 
          var weatherCard = document.getElementById('weatherCard');
@@ -16,8 +19,7 @@ function weatherData(){
             </div>
           </div>`
     })
-
-    
-    }
+})
+}
     
   
